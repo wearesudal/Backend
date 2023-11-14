b0b1e0f2616c2d5ad3ccfc1a1cd7211fe4ad569c
이경민 <kmeye0419@gmail.com>
2023-11-14 16:33:36 +0900

    chore #1 : 프로젝트 파일 추가

---
+++ HouseBackend/src/main/java/com/sudal/home/HouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sudal.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseBackendApplication.class, args);
+	}
+
+}
---
+++ HouseBackend/src/test/java/com/sudal/home/HouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudal.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
