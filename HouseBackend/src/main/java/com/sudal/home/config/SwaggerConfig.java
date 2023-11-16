23960a52022e78aca1e6295ed2f0c1b563d073a0
choeyicho <choeyicho1105@gmail.com>
2023-11-16 17:30:35 +0900

    chore #2 : build.gradle 수정

--- HouseBackend/src/main/java/com/sudal/home/config/SwaggerConfig.java
+++ HouseBackend/src/main/java/com/sudal/home/config/SwaggerConfig.java
@@ -16,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
