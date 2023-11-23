bd3a176495c9f7538e188a6803d8e40bf0365f73
이경민 <kmeye0419@gmail.com>
2023-11-23 17:30:33 +0900

    feat #17 : 시,군,구 조회 API 보완 구현

--- HouseBackend/src/main/java/com/sudal/home/domain/map/controller/MapController.java
+++ HouseBackend/src/main/java/com/sudal/home/domain/map/controller/MapController.java
@@ -2,7 +2,6 @@ package com.sudal.home.domain.map.controller;
 
 import com.sudal.home.common.GeneralResponse;
 import com.sudal.home.common.ResponseCode;
-import com.sudal.home.domain.map.dto.request.MapGugunInfoRequestDto;
 import com.sudal.home.domain.map.dto.request.MapDongInfoRequestDto;
 import com.sudal.home.domain.map.dto.request.MapSearchRequestDto;
 import com.sudal.home.domain.map.service.MapService;
@@ -10,16 +9,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/map")
 @RequiredArgsConstructor
 public class MapController {
 
     private final MapService mapService;
-
     @GetMapping("")
-    public ResponseEntity<GeneralResponse> selectByCondition(@RequestBody MapSearchRequestDto mapSearchRequestDto) {
-        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectByCondition(mapSearchRequestDto));
+    public ResponseEntity<GeneralResponse> selectByCondition(@RequestParam("sidoName") String sidoName,
+                                                             @RequestParam("gugunName") String gugunName,
+                                                             @RequestParam("dongName") String dongName,
+                                                             @RequestParam("dealYear") Integer dealYear,
+                                                             @RequestParam("dealMonth") Integer dealMonth) {
+        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectByCondition(new MapSearchRequestDto(sidoName, gugunName, dongName, dealYear, dealMonth)));
     }
 
     @GetMapping("/detail/{no}")
@@ -33,12 +36,12 @@ public class MapController {
     }
 
     @GetMapping("/info/gugun")
-    public ResponseEntity<GeneralResponse> selectAllGugunName(@RequestBody MapGugunInfoRequestDto mapCityInfoRequestDto) {
-        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectAllGugunName(mapCityInfoRequestDto));
+    public ResponseEntity<GeneralResponse> selectAllGugunName(@RequestParam String cityName) {
+        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectAllGugunName(cityName));
     }
 
     @GetMapping("/info/dong")
-    public ResponseEntity<GeneralResponse> selectAllDongName(@RequestBody MapDongInfoRequestDto MapDongInfoRequestDto) {
-        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectAllDongName(MapDongInfoRequestDto));
+    public ResponseEntity<GeneralResponse> selectAllDongName(@RequestParam String cityName, @RequestParam String gugunName) {
+        return GeneralResponse.success(ResponseCode.SUCCESS, mapService.selectAllDongName(new MapDongInfoRequestDto(cityName, gugunName)));
     }
 }
